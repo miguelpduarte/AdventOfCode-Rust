43fcd614461c6b657b82f3c40f0213aea3fe687f
Miguel Duarte <miguelpduarte98@gmail.com>
2024-12-09 01:36:22 +0100

    2024d8-p1: Initial solution that overshoots only on real input

---
+++ aoc2024/src/bin/08.rs
@@ -0,0 +1,209 @@
+#![feature(test)]
+extern crate test;
+
+use std::collections::{HashMap, HashSet};
+
+type TowerType = char;
+
+#[derive(Debug, PartialEq)]
+struct Tower {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(PartialEq, Eq, Hash, Debug)]
+struct AntiNode {
+    pub x: usize,
+    pub y: usize,
+}
+
+fn solve_day(input: String) -> (usize, usize) {
+    let mut towers_by_type: HashMap<TowerType, Vec<Tower>> = HashMap::new();
+    let mut height = 0;
+    // TODO: Maybe those OnceCell can make this not repeat a calculation?
+    // Should probably not be a bottleneck though.
+    let mut width = 0;
+
+    for (y, line) in input.lines().enumerate() {
+        // TODO: We could also iterate and get this rather than using str::len().
+        // Not sure if that would necessarily be better, as this might be cached,
+        // but since we iterate anyway...
+        width = line.len();
+        height += 1;
+
+        // Parse towers
+        // Assume ASCII chars for speed.
+        for (x, item) in line.bytes().enumerate() {
+            let item = item as char;
+            if item != '.' {
+                towers_by_type.entry(item).or_default().push(Tower { x, y });
+            }
+        }
+    }
+
+    println!("{:?}", towers_by_type);
+
+    let n_towers: usize = towers_by_type.values().map(|towers| towers.len()).sum();
+    println!("n towers: {}", n_towers);
+
+    let antinodes: HashSet<AntiNode> = calculate_antinodes(&towers_by_type, width, height);
+
+    let _: Vec<_> = antinodes
+        .iter()
+        .inspect(|an| println!("{:?}", an))
+        .collect();
+
+    let p1 = antinodes.len();
+
+    let p2 = 0;
+
+    (p1, p2)
+}
+
+fn calculate_antinodes(
+    towers: &HashMap<char, Vec<Tower>>,
+    width: usize,
+    height: usize,
+) -> HashSet<AntiNode> {
+    let mut antinodes = HashSet::new();
+
+    for towers in towers.values() {
+        // For each tower, compute all possible antinodes by matching with all towers (other than
+        // itself)
+
+        for our_tower in towers {
+            for their_tower in towers {
+                if our_tower == their_tower {
+                    continue;
+                }
+
+                if let Some(antinode) =
+                    calculate_antinode_bounded(our_tower, their_tower, width, height)
+                {
+                    antinodes.insert(antinode);
+                }
+            }
+        }
+    }
+
+    antinodes
+}
+
+fn calculate_antinode_bounded(
+    ours: &Tower,
+    theirs: &Tower,
+    width: usize,
+    height: usize,
+) -> Option<AntiNode> {
+    // Calculate the vector between our_tower and their_tower and double it.
+    // Always use the same direction (our to their) because we are doing all possible combinations,
+    // so we will cover the reverse case already.
+
+    // x = ours.x + (theirs.x - ours.x) * 2
+    // <> 2*theirs.x - ours.x (aka theirs.x + dif)
+    let x = (2 * theirs.x).checked_sub(ours.x)?;
+    let y = (2 * theirs.y).checked_sub(ours.y)?;
+
+    println!("Trying {},{}", x, y);
+
+    // checked_sub handles left and top bound, but the others have to be checked manually:
+    if x > width || y > height {
+        return None;
+    }
+
+    Some(AntiNode { x, y })
+}
+
+#[test]
+fn example_input() {
+    let input = "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"
+        .to_owned();
+    let res = solve_day(input);
+    assert_eq!(res.0, 14);
+    // assert_eq!(res.1, 11387);
+}
+
+#[test]
+fn small_example_input() {
+    let input = "..........
+..........
+..........
+....a.....
+........a.
+.....a....
+..........
+..........
+..........
+.........."
+        .to_owned();
+    let res = solve_day(input);
+    assert_eq!(res.0, 4);
+    // assert_eq!(res.1, 11387);
+}
+
+#[test]
+fn small_example_input_with_big_a() {
+    let input = "..........
+..........
+..........
+....a.....
+........a.
+.....a....
+..........
+......A...
+..........
+.........."
+        .to_owned();
+    let res = solve_day(input);
+    assert_eq!(res.0, 4);
+    // assert_eq!(res.1, 11387);
+}
+
+#[test]
+fn small_manual_example_input_test_underflow() {
+    let input = "aaZ.
+..Z.
+....
+...."
+        .to_owned();
+    let res = solve_day(input);
+    assert_eq!(res.0, 2);
+    // assert_eq!(res.1, 11387);
+}
+
+#[test]
+fn small_manual_example_input_test_oob_diagonal_bottom_left() {
+    let input = ".....
+.....
+..N..
+.....
+N...."
+        .to_owned();
+    let res = solve_day(input);
+    assert_eq!(res.0, 1);
+    // assert_eq!(res.1, 11387);
+}
+
+#[test]
+fn prod_solution() {
+    use std::fs::read_to_string;
+
+    let input = read_to_string(format!("inputs/{}", "8.in")).unwrap();
+    let res = solve_day(input);
+    // 366 is too high
+    assert_eq!(res.0, 42);
+    assert_eq!(res.1, 42);
+}
+
+aoc2024::day_main!("8.in");
