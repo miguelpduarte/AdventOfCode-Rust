513009d3b9288bf04a1be611362a4c972afb6158
Miguel Duarte <miguelpduarte98@gmail.com>
2024-12-13 01:20:36 +0100

    2024-d10p2: That was easy. I had accidentally done it in p1

--- aoc2024/src/bin/10.rs
+++ aoc2024/src/bin/10.rs
@@ -5,6 +5,7 @@ const MAX_GRID_SIZE: usize = 50;
 
 fn solve_day(input: String) -> (usize, usize) {
     let mut trailhead_locs = vec![];
+    let mut peak_locs = vec![];
 
     // Just assuming that it's a square, aligns with input and example
     let grid_size = input.lines().next().unwrap().len();
@@ -26,20 +27,91 @@ fn solve_day(input: String) -> (usize, usize) {
             if height == 0 {
                 // Trailhead detected, cache for easier access
                 trailhead_locs.push((x, y));
+            } else if height == 9 {
+                // Cache peaks for easier access for p2 as well.
+                peak_locs.push((x, y));
             }
         }
     }
 
     let trailhead_scores = trailhead_locs
-        .into_iter()
-        .map(|trailhead| calc_trailhead_score(trailhead, &grid))
+        .iter()
+        .map(|trailhead| calc_trailhead_score(*trailhead, &grid))
         .sum();
 
     let p1 = trailhead_scores;
 
-    let p2 = 0;
+    let trailhead_ratings = trailhead_locs
+        .iter()
+        .map(|trailhead| calc_trailhead_rating(*trailhead, &grid))
+        .sum();
+
+    let p2 = trailhead_ratings;
 
     (p1, p2)
+}
+
+/// Lmao, part 2 is actually solvable by just removing the repeated path checking I had for part 1.
+/// (Literally the only difference between the two is not having the set to track the duplicate
+/// visits to a node, which I had accidentally forgot for part 1 initially).
+fn calc_trailhead_rating(
+    trailhead_loc: (usize, usize),
+    grid: &[[u8; MAX_GRID_SIZE]; MAX_GRID_SIZE],
+) -> usize {
+    let mut rating = 0;
+
+    // Start at trailhead, and then fan out (BFS, FIFO)
+    let mut to_visit = vec![trailhead_loc];
+
+    while let Some((curr_x, curr_y)) = to_visit.pop() {
+        let curr_val = grid[curr_y][curr_x];
+
+        if curr_val == 9 {
+            // Trailhead got us to another peak
+            rating += 1;
+            // Since we cannot go upwards from 9, we don't need to explore anymore
+            continue;
+        }
+
+        let target_value = curr_val + 1;
+
+        let candidate_neighbour_coords = [
+            // TODO: Maybe extract this to const, maybe it gets compiled in and is faster? probably
+            // overengineering the optimization though.
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1),
+        ]
+        .into_iter()
+        // Calculate neighbour coordinates with underflow check
+        .filter_map(|(x_shift, y_shift)| {
+            Some((
+                curr_x.checked_add_signed(x_shift)?,
+                curr_y.checked_add_signed(y_shift)?,
+            ))
+        })
+        // Check outer bounds
+        .filter(|(x, y)| {
+            // The bound of being > 0 is already checked via usize::checked_add_signed, as it would
+            // underflow
+            // TODO: Technically we could use the parsed grid_size here to cut some exploration,but
+            // it's unlikely to be relevant, as the real input would be the slower case and there
+            // we use the full grid so this would be correct.
+            *x < MAX_GRID_SIZE && *y < MAX_GRID_SIZE
+        })
+        // Check if the candidate neighbour has our target value (1 higher than the current value)
+        .filter(|(x, y)| grid[*y][*x] == target_value);
+
+        // Add all candidate neighbours to the exploration queue
+        for candidate_neighbour_coords in candidate_neighbour_coords {
+            to_visit.push(candidate_neighbour_coords);
+        }
+    }
+
+    // When the loop is done, we have checked all paths. Since we need to strictly grow, we cannot
+    // have cycles - so we don't need to worry about tracking what we already visited.
+    rating
 }
 
 fn calc_trailhead_score(
@@ -123,7 +195,7 @@ fn example_input() {
         .to_owned();
     let res = solve_day(input);
     assert_eq!(res.0, 36);
-    // assert_eq!(res.1, 2858);
+    assert_eq!(res.1, 81);
 }
 
 #[test]
@@ -132,8 +204,8 @@ fn prod_solution() {
 
     let input = read_to_string(format!("inputs/{}", "10.in")).unwrap();
     let res = solve_day(input);
-    assert_eq!(res.0, 42);
-    assert_eq!(res.1, 42);
+    assert_eq!(res.0, 629);
+    assert_eq!(res.1, 1242);
 }
 
 aoc2024::day_main!("10.in");
