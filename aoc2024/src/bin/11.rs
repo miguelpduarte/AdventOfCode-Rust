b3acfa7e2185efa8dfe2a187fd1672c8bf845f89
Miguel Duarte <miguelpduarte98@gmail.com>
2024-12-13 10:43:21 +0100

    2024d11: Try to optimize but create new bug

--- aoc2024/src/bin/11.rs
+++ aoc2024/src/bin/11.rs
@@ -3,6 +3,16 @@
 use std::collections::HashMap;
 extern crate test;
 
+// TODO: For some reason, this is still 4ms. Might have been due to the other janky solution being
+// running at the time, but it might need some shaving off.
+// Likely would be doable by reusing the same map rather than basically cloning it every time. But
+// then we need to iterate over the map while changing it at the same time, which I'm not sure is
+// really possible... And if we clone it to then modify it based on the "view-only" copy, it's
+// basically the same as creating a new map every time!
+//
+// I guess maybe we can get away with just cloning a list of keys and then accessing those and
+// changing them? But that should not be so different...
+
 fn solve_day(input: String) -> (usize, usize) {
     // Maps <stone_value, stone_count>
     let mut stones_frequency: HashMap<usize, usize> = HashMap::new();
@@ -15,17 +25,17 @@ fn solve_day(input: String) -> (usize, usize) {
         *stones_frequency.entry(stone_value).or_default() += 1;
     }
 
-    // println!("start: {:?}", stones_frequency);
+    println!("start: {:?}", stones_frequency);
 
     for _i in 0..25 {
-        stones_frequency = blink(stones_frequency);
-        // println!("{_i}: {:?}", stones_frequency);
+        blink(&mut stones_frequency);
+        println!("{_i}: {:?}", stones_frequency);
     }
 
     let p1 = stones_frequency.values().sum();
 
     for _i in 0..50 {
-        stones_frequency = blink(stones_frequency);
+        blink(&mut stones_frequency);
     }
 
     let p2 = stones_frequency.values().sum();
@@ -33,33 +43,41 @@ fn solve_day(input: String) -> (usize, usize) {
     (p1, p2)
 }
 
-fn blink(stones: HashMap<usize, usize>) -> HashMap<usize, usize> {
-    // Honestly it's probably just easier to always make a new HashMap, so that we don't have to
-    // worry with mid-loop changing values and stuff.
+fn blink(stones: &mut HashMap<usize, usize>) {
+    // BUG: Since we are not preserving the stones order and making the change in-place, we are
+    // working on intermediate updated values, so we might transform more stones than we actually
+    // should, because they should not have had that value yet.
+    //
+    // Ideas for solutions:
+    // 1. Use two maps and just switch back and forth. This reduces allocations I guess but ends up
+    //    being similar to the original solution. Here we would just be .clear()ing the output map
+    //    each time and swapping them around I guess.
+    // 2. Output all the changes into a list, and only "commit" them later, all in one go.
 
-    let mut new_stones: HashMap<usize, usize> = HashMap::with_capacity(stones.len());
+    let existing_stone_values: Vec<usize> = stones.keys().cloned().collect();
 
-    for (value, count) in stones {
+    for value in existing_stone_values {
+        // Almost like a .pop for a queue, so that we don't need to reset it later and minimize
+        // used space.
+        let count = stones.remove(&value).unwrap();
+
         // Rule 1: 0->1
         if value == 0 {
-            *new_stones.entry(1).or_default() += count;
+            *stones.entry(1).or_default() += count;
             continue;
         }
 
         // Rule 2: even digits = split off
         // This means that we will have count stones with each of the values
         if let Some((stone1_value, stone2_value)) = split_if_even_digits(value) {
-            *new_stones.entry(stone1_value).or_default() += count;
-            *new_stones.entry(stone2_value).or_default() += count;
-
+            *stones.entry(stone1_value).or_default() += count;
+            *stones.entry(stone2_value).or_default() += count;
             continue;
         }
 
         // Rule 3: value*2024
-        *new_stones.entry(value * 2024).or_default() += count;
+        *stones.entry(value * 2024).or_default() += count;
     }
-
-    new_stones
 }
 
 fn split_if_even_digits(n: usize) -> Option<(usize, usize)> {
